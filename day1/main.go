67b177ec6246e8f568f2852120cd2cdfff7be56a
mihsamusev <mih.samusev@gmail.com>
2023-12-02 11:33:45 +0100

    day 1 part 1 completed

--- day1/main.go
+++ day1/main.go
@@ -1,13 +1,62 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
 
-dataFile := "test.txt"
 
-func parse(dataFile string) {
-
+func to_digit(r rune) int {
+	return int(r - '0')
 }
 
+func line_value(str string) int {
+	value := 0
+	digits := make([]int, 0, 10)
+	for _, rune := range str {
+		if unicode.IsDigit(rune) {
+			digits = append(digits, to_digit(rune))
+		}
+	}
+	numDigits := len(digits)
+	if numDigits != 0 {
+		first := digits[0]
+		last := digits[numDigits - 1]
+		value = first*10 + last
+	}
+
+	return value
+}
+
+func parse(dataFile string) {
+	file, err := os.Open(dataFile)
+	if err != nil {
+		fmt.Println("im dead")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	total := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		value := line_value(line)
+		total += value
+		fmt.Printf("%s -> %d\n", line, value)
+	}
+	fmt.Printf("total = %d\n", total)
+	
+
+	defer file.Close()
+}
 
 func main() {
-
+	dataFile := "test.txt"
+	args := os.Args
+	if len(args) > 1 {
+		dataFile = args[1]
+	}
+	parse(dataFile)
 }
